35490e26d7fbb381eaa098fbb4859771457d262f
Kyle <kkingsbe@gmail.com>
2020-08-17 00:00:32 -0400

    Fixed mesage

--- smart-mirror/src/components/message.svelte
+++ smart-mirror/src/components/message.svelte
@@ -4,12 +4,13 @@
   setInterval(() => {
     date = new Date()
     let hour = date.getHours()
+    console.log(hour)
 
     if(hour >= 5 && hour < 12) {
       greetingText = "Good Morning :)"
     } else if(hour >=12 && hour < 18) {
       greetingText = "Good Afternoon :)"
-    } else if(hour >= 18 && hour < 5) {
+    } else if(hour >= 18 || hour < 5) {
       greetingText = "Good Night :)"
     }
   }, 1000)
