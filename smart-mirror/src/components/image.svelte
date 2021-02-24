12900b4ca6dbcd7e1e8dd6d3dd76f697d9dfe45c
kkingsbe <kkingsbe@gmail.com>
2021-02-23 19:26:37 -0500

    Replaced APOD with image until NASA adds percy to the API

---
+++ smart-mirror/src/components/image.svelte
@@ -0,0 +1,18 @@
+<script>
+    
+  </script>
+  
+  <main>
+    <img src="https://pbs.twimg.com/media/Eu8287dWYAItUvT?format=jpg&name=4096x4096">
+  </main>
+  
+  <style>
+    main {
+      position: fixed;
+      bottom: 250px;
+      width: 100%;
+    }
+    img {
+      width: 60%;
+    }
+  </style>
