bde0ef1fe8b051d0b599d348e18c5161a057c517
kkingsbe <kkingsbe@gmail.com>
2021-10-25 13:38:25 -0400

    Fixed a minor oofie

--- smart-mirror/src/components/for_sarah.svelte
+++ smart-mirror/src/components/for_sarah.svelte
@@ -1,6 +1,6 @@
 <script>
     //version = aws-sdk-2.985.0.min.js
-    var picture_names = ["IMG-0339.jpg", "IMG-0374.JPG", "IMG-0392.jpg", "IMG-0398.JPG", "IMG-0400.JPG", "IMG-9269.JPG", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpeg", "13.jpeg", "14.jpeg", "15.jpeg", "16.jpeg", "17.JPG", "18.PNG", "19.PNG", "20.JPG", "21.PNG", "22.JPG", "23.JPG", "24.JPG", "25.PMG", "26.JPG"]
+    var picture_names = ["IMG-0339.jpg", "IMG-0374.JPG", "IMG-0392.jpg", "IMG-0398.JPG", "IMG-0400.JPG", "IMG-9269.JPG", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpeg", "13.jpeg", "14.jpeg", "15.jpeg", "16.jpeg", "17.JPG", "18.PNG", "19.PNG", "20.JPG", "21.PNG", "22.JPG", "23.JPG", "24.JPG", "25.PNG", "26.JPG"]
     var bucket = "https://smart-mirror-pictures.s3.amazonaws.com/"
     var i = 0
     var sarah_picture = bucket + picture_names[i]
