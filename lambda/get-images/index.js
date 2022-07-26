391ed9d97931aabf6e889b6d67e8702e4dd684e3
kkingsbe <kkingsbe@gmail.com>
2022-07-25 20:05:30 -0400

    Removed key

--- lambda/get-images/index.js
+++ lambda/get-images/index.js
@@ -1,7 +1,5 @@
 const mysql = require("mysql2")
 
-const DATABASE_URL = 'mysql://lnjvzmfjx2xs:pscale_pw_-0SK_oMK_JG-6ywbY4zxKFkirb1MOiw566nocrrwAis@xvvb2acg505j.us-east-3.psdb.cloud/smart-mirror?ssl={"rejectUnauthorized":true}'
-
 exports.handler = async (event) => {
     const id = event.queryStringParameters.id
 
